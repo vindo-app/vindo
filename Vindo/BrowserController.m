39609376710e3d9c9ee17ef5664e5fb2a466e0c3
Theodore Dubois <tblodt@icloud.com>
2015-04-09 16:05:16 -0700

    much working work on browser

--- Vindo/BrowserController.m
+++ Vindo/BrowserController.m
@@ -38,36 +38,6 @@ static NSMutableArray *browsers;
     [self.tabView selectTabViewItemAtIndex:sender.selectedSegment];
 }
 
-#pragma mark NSBrowserDelegate
-
-- (Item *)rootItemForBrowser:(NSBrowser *)browser {
-    return self.rootItem;
-}
-
-- (BOOL)browser:(NSBrowser *)browser isLeafItem:(Item *)item {
-    return item.leaf;
-}
-
-- (NSInteger)browser:(NSBrowser *)browser numberOfChildrenOfItem:(Item *)item {
-    return item.children.count;
-}
-
-- (Item *)browser:(NSBrowser *)browser child:(NSInteger)index ofItem:(Item *)item {
-    return item.children[index];
-}
-
-- (id)browser:(NSBrowser *)browser objectValueForItem:(Item *)item {
-    return [NSNull null];
-}
-
-- (void)browser:(NSBrowser *)sender willDisplayCell:(ItemBrowserCell *)cell atRow:(NSInteger)row column:(NSInteger)column {
-    Item *item = [sender itemAtRow:row inColumn:column];
-    cell.stringValue = item.name;
-    cell.image = item.image;
-}
-
-#pragma mark Other Stuff
-
 - (void)windowWillClose:(NSNotification *)notification {
     [browsers removeObject:self]; // get rid of the strong reference
 }
---
+++ Vindo/BrowserDelegate.h
@@ -0,0 +1,14 @@
+//
+//  BrowserDelegate.h
+//  Vindo
+//
+//  Created by Dubois, Theodore Alexander on 4/9/15.
+//  Copyright (c) 2015 Theodore Dubois. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "BrowserController.h"
+
+@interface BrowserDelegate : NSObject <NSBrowserDelegate>
+
+@end
