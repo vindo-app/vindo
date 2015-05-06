c71b77e277e6ddd8243d3d942a9e1de37cecbb6c
Theodore Dubois <tblodt@icloud.com>
2015-05-05 17:30:48 -0700

    working on sidebar

---
+++ Vindo/SidebarController.m
@@ -0,0 +1,13 @@
+//
+//  SidebarController.m
+//  Vindo
+//
+//  Created by Dubois, Theodore Alexander on 5/5/15.
+//  Copyright (c) 2015 Theodore Dubois. All rights reserved.
+//
+
+#import "SidebarController.h"
+
+@implementation SidebarController
+
+@end
---
+++ Vindo/SidebarGroup.m
@@ -0,0 +1,13 @@
+//
+//  SidebarGroup.m
+//  Vindo
+//
+//  Created by Dubois, Theodore Alexander on 5/5/15.
+//  Copyright (c) 2015 Theodore Dubois. All rights reserved.
+//
+
+#import "SidebarGroup.h"
+
+@implementation SidebarGroup
+
+@end
