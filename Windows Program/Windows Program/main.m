98aa9a5595415bc26d1b84c0d25713432a92c7a1
Theodore Dubois <tblodt@icloud.com>
2015-12-18 16:43:14 -0800

    add app bundle target

---
+++ Windows Program/Windows Program/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Windows Program
+//
+//  Created by Dubois, Theodore Alexander on 12/18/15.
+//  Copyright © 2015 Theodore Dubois. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
