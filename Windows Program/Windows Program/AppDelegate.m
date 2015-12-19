70e73f9dad27f5e6ef62c9f7baaf5d756c210657
Theodore Dubois <tblodt@icloud.com>
2015-12-18 16:43:14 -0800

    add app bundle target

---
+++ Windows Program/Windows Program/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  Windows Program
+//
+//  Created by Dubois, Theodore Alexander on 12/18/15.
+//  Copyright © 2015 Theodore Dubois. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
