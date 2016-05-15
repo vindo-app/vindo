cffacbae289042a1e8f6df9b4f04b6e6112f5809
Theodore Dubois <tblodt@icloud.com>
2016-05-14 17:27:14 -0700

    fuzzy searching

--- js/compatibility.js
+++ js/compatibility.js
@@ -1,15 +1,23 @@
-var db;
+var db = [];
 var $results;
 var $search_box;
 var results;
 
+function buildDatabase() {
+    $(".compatibility-entry").each(function() {
+        $entry = $(this);
+        db.push({
+            "name": $entry.data("name"),
+            "keywords": $entry.data("keywords"),
+            "html": $entry[0].outerHTML
+        });
+    });
+}
+
 function loadEntries() {
     $results.empty();
     for (var i = 0; i < results.length && i < 10; i++) {
-        var entry = results[i];
-        var $placeholder = $("<div></div>");
-        $results.append($placeholder);
-        $placeholder.html(entry.html);
+        $results.append($(results[i].html));
     }
 }
 
@@ -19,19 +27,15 @@ $(document).ready(function() {
         return;
     
     $(".js-only").removeClass("js-only");
+
+    buildDatabase();
     
-    $.getJSON("/compat.json", function(response) {
-        db = response;
-        results = db;
-        loadEntries();
-    });
-    
-    var $search_box = $("#search-box");
-    $search_box.keyup(function(event) {
-        var regex = new RegExp($search_box.val(), "i");
-        results = db.filter(function(entry) {
-            return regex.test(entry.name);
+    $("#search-box").keyup(function(event) {
+        var fuse = new Fuse(db, {
+            keys: ["name", "keywords"]
         });
+        results = fuse.search($(this).val());
+        console.log(results);
         loadEntries();
     });
 });
