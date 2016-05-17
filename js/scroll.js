2491d29f4d48c5933ab28b436458a3bffa09ef78
Theodore Dubois <tblodt@icloud.com>
2016-05-17 14:07:13 -0700

    compatibility gallery

--- js/scroll.js
+++ js/scroll.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $("a[href^=\"#\"]").click(function(event) {
         event.preventDefault();
         $("html, body").animate({
-            scrollTop: $(".page").next().offset().top
+            scrollTop: $($(this).attr("href")).offset().top - 50 // 50 for the navbar
         }, 1000);
     });
 });
